984b316ad4420206e962265312c2a977d6a09313
Ivan Matkov <ivan.matkov@jetbrains.com>
2024-02-22 13:18:09 +0100

    Add js and wasm for lifecycle modules

--- lifecycle/lifecycle-runtime/src/webMain/kotlin/androidx/lifecycle/LifecycleRegistry.web.kt
+++ lifecycle/lifecycle-runtime/src/webMain/kotlin/androidx/lifecycle/LifecycleRegistry.web.kt
@@ -17,11 +17,10 @@
 package androidx.lifecycle
 
 internal actual class WeakReference<T : Any> actual constructor(
-    reference: T
+    private val reference: T
 ) {
-    private val kotlinNativeReference = kotlin.native.ref.WeakReference(reference)
-    actual fun get(): T? = kotlinNativeReference.get()
+    actual fun get(): T? = reference
 }
 
 internal actual fun isMainThread(): Boolean =
-    MainDispatcherChecker.isMainDispatcherThread()
+    true
